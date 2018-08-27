9c4fc1aea3cb4c8ec91a9c5daddcee1d73f1ef8c
CarlJacobs1 <carl.jacobs3@gmail.com>
2018-08-27 20:03:07 +0200

    Add password to userCreation class

--- src/Pages/newUser.php
+++ src/Pages/newUser.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>New User Registration</title>
+	<script type="text/javascript" src="./JS/NewUser/NewUser.js"></script>
 </head>
 <body>
 	<div>
@@ -56,62 +57,7 @@
 	<?php
 	include('../Include/users/newUserRegistration.php');
 	?>
-	<script type="text/javascript">
-
-		function validateFormData(){
-			var username = document.getElementById('username').value;
-			var first_name = document.getElementById('first_name').value;
-			var last_name = document.getElementById('last_name').value;
-			var password = document.getElementById('password').value;
-			var password2 = document.getElementById('password2').value;
-			var password_confirmation_label = document.getElementById('password_confirmation_label');
-			var createUser = document.getElementById('createUser');
-			var validationBoolean = 1;
-
-			if (username == '') {
-				validationBoolean = 0;
-			}
-
-			if (first_name == '') {
-				validationBoolean = 0;
-			}
-
-			if (last_name == '') {
-				validationBoolean = 0;
-			}
-
-			if (password == '') {
-				validationBoolean = 0;
-			}
-
-			if (password2 == '') {
-				validationBoolean = 0;
-			}
-
-			if (password2 == '') {
-				password_confirmation_label.setAttribute('hidden','hidden')
-			} else{
-				password_confirmation_label.removeAttribute('hidden','hidden')
-				if (password !== password2) {
-					password_confirmation_label.innerHTML = 'Passwords do not match';
-					validationBoolean = 0;
-
-				} else{
-					password_confirmation_label.innerHTML = 'Passwords Match!';
-				}
-			}
-
-			if (validationBoolean == 0) {
-				createUser.setAttribute('disabled', 'disabled');
-
-			} else{
-				createUser.removeAttribute('disabled', 'disabled');
-
-			}
-
-		}
-
-	</script>
+	
 	
 </body>
 </html>
