59b57f0a6ddeeff505b38f45e738d2362ceb6280
CarlJacobs1 <carl.jacobs3@gmail.com>
2018-09-04 21:21:26 +0200

    Updated User Login Page and User Creation Page to use Material Design Lite

--- src/Pages/login.php
+++ src/Pages/login.php
@@ -3,10 +3,47 @@
 <head>
 	<title>The Bear Cave Login</title>
 	<link rel="icon" type="image/png" href="./favIcon.png">
-	<link rel="stylesheet" type="text/css" href="./CSS/login.css">
+	<link rel="stylesheet" href="./CSS/material.min.css">
+	<script src="./CSS/material.min.js"></script>
 </head>
 <body>
-	
+
+	<div class="mdl-layout mdl-js-layout">
+		<div class="mdl-card mdl-shadow--6dp">
+			<div class="mdl-card__title mdl-color--primary mdl-color-text--white" >
+				<h2 class="mdl-card__title-text">Bear Cave Entrance</h2>
+			</div>
+			<form action="login.php" method="post">
+				<div class="mdl-card__actions">
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+						<input  class="mdl-textfield__input" type="text" name="username" id="username">
+						<label class="mdl-textfield__label" for="username"> Username</label>
+					</div>
+
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+						<input  class="mdl-textfield__input" type="password" name="password" id="password">
+						<label class="mdl-textfield__label" for="password">Password</label>
+					</div>
+
+				</div>
+				<div class="mdl-card__actions">
+					<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored type="submit"  name='login'>Login</button>
+					<a href="./newUser.php">Register New User</a>
+
+				</div>
+			</form>
+			<?php
+			include('../Include/loginHandler.php');
+
+			?>
+			
+		</div>
+		
+
+	</div>
+
+	<!--
 	<div class="login">
 		<div class="heading">
 			<h2>Sign in</h2>
@@ -25,13 +62,11 @@
 			</form>
 			<a href="./newUser.php">Register New User</a>
 		</div>
-		<?php
-		include('../Include/loginHandler.php');
-
-		?>
+		
 	</div>
+-->
 
-	
+
 </body>
 
 
--- src/Pages/newUser.php
+++ src/Pages/newUser.php
@@ -2,62 +2,107 @@
 <html>
 <head>
 	<title>New User Registration</title>
-	<script type="text/javascript" src="./JS/NewUser/NewUser.js"></script>
+	<link rel="stylesheet" href="./CSS/material.min.css">
+	<script src="./CSS/material.min.js"></script>
+	<script  src="./JS/NewUser/newUser.js"></script>	
 </head>
 <body>
+	<div class="mdl-layout mdl-js-layout">
+		<div class="mdl-card mdl-shadow--6dp">
+			<div class="mdl-card__title mdl-color--primary mdl-color-text--white" >
+				<h2 class="mdl-card__title-text">Enter User Details</h2>
+			</div>
+			
+			<form action="newUser.php" method="post">
+				<div class="mdl-card__actions">
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+						<input class="mdl-textfield__input" type="text" name="username" id="username" onkeyup="validateFormData()">
+						<label class="mdl-textfield__label" for="username">Email Address *: </label>
+					</div>
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+						<input class="mdl-textfield__input" type="text" name="first_name" id="first_name" onkeyup="validateFormData()">
+						<label class="mdl-textfield__label" for="first_name">First Name *:</label>
+					</div>
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+						<input class="mdl-textfield__input" type="text" name="second_name" id="second_name" onkeyup="validateFormData()">
+						<label class="mdl-textfield__label" for="second_name">Second Name:</label>
+					</div>
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+						<input class="mdl-textfield__input" type="text" name="last_name" id="last_name" onkeyup="validateFormData()">
+						<label class="mdl-textfield__label" for="last_name">Last Name *:</label>
+					</div>
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+						<input class="mdl-textfield__input" type="password" name="password" id="password" onkeyup="validateFormData()">
+						<label class="mdl-textfield__label" for="password">Password *:</label>
+					</div>
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+						<input class="mdl-textfield__input"  type="password" name="password2" id="password2" onkeyup="validateFormData()">
+						<label class="mdl-textfield__label" for="password2">Confirm Password *: </label>
+					</div>
+					<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" type="submit"  name='createUser' id="createUser" disabled="disabled">Submit</button>
+					<label name="password_confirmation_label" id="password_confirmation_label"></label>
+				</div>
+			</form>
+			<?php
+			include('../Include/users/newUserRegistration.php');
+			?>
+		</div>
+
+	</div>
+
+	
+
+<!--
 	<div>
 		<h2>New User Registration</h2>
 		<form action="newUser.php" method="post">
 			<p>
 				<span>
-					<label>Email Address *: </label>
-					<input type="text" name="username" id="username" onkeyup="validateFormData()">
+					
 				</span>
 			</p>
 			<p>
 				<span> 
-					<label>First Name *:</label>
-					<input type="text" name="first_name" id="first_name" onkeyup="validateFormData()">
+					
 				</span>
 			</p>
 
 			<p>
 				<span>
-					<label>Second Name:</label>
-					<input type="text" name="second_name" id="second_name" onkeyup="validateFormData()">
-				</span>
-			</p>
-			<p>
-				<span>
-					<label>Last Name *:</label>
-					<input type="text" name="last_name" id="last_name" onkeyup="validateFormData()">
-				</span>
-			</p>
-			<p>
-				<span>
-					<label>Password *:</label>
-					<input type="password" name="password" id="password" onkeyup="validateFormData()">
+					
 				</span>
 			</p>
 			<p>
 				<span>
 					
-					<label>Confirm Password *: </label>
-					<input type="password" name="password2" id="password2" onkeyup="validateFormData()">
 				</span>
 			</p>
 			<p>
-				<label name="password_confirmation_label" id="password_confirmation_label"></label>
+				<span>
+					
+				</span>
+			</p>
+			<p>
+				<span>
+					
+					
+				</span>
+			</p>
+			<p>
+				
 			</p>
 			<p>
 				<input type="submit" name="createUser" id="createUser" disabled="disabled">
 			</p>
 		</form>
 	</div>
-	<?php
-	include('../Include/users/newUserRegistration.php');
-	?>
-	
-	
+-->
+
+
 </body>
 </html>
